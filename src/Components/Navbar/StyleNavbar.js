b12cbb9f85eb5319edf02442cc89f76fae493ad0
vladie777 <diego.800luis@gmail.com>
2023-05-08 12:50:05 -0500

    cambios css

--- src/Components/Navbar/StyleNavbar.js
+++ src/Components/Navbar/StyleNavbar.js
@@ -5,48 +5,75 @@ export const StylesNav = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 40px 70px 0;
+  cursor: pointer;
+  .logo {
+    margin-left: -22px;
+  }
   @media screen and (max-width: 376px) {
     padding: 40px 20;
     flex-direction: column;
-    background-color: ${props => props.viewOption ? 'black' : 'inherit'};
+    background-color: ${(props) => (props.viewOption ? "black" : "inherit")};
     margin-left: -77px;
-   
+  }
+  .logo {
+    margin-left: -40px;
   }
 `;
 
-export const Ul = styled.ul `
+export const Ul = styled.ul`
   list-style: none;
   display: flex;
   gap: 25px;
   align-items: center;
   color: white;
   font-size: 18px;
-  font-family: 'Josefin Sans', sans-serif;
-  
-  
- 
+  text-align: right;
+  li {
+    ::after {
+      content: "";
+      height: 2px;
+      width: 0;
+      background-color: white;
+      display: block;
+      transition: all 300ms;
+      display: flex;
+      margin: auto;
+      margin-top: 5px;
+      font-family: "Josefin Sans", sans-serif;
+    }
+
+    :hover::after {
+      width: 20px;
+    }
+  }
+
   @media screen and (max-width: 376px) {
-  //Evaluar si viewoption es true se aplica flex y si es false se aplica none
-  //https://styled-components.com/docs/basics#adapting-based-on-props
-    display: ${props => props.viewOption ? 'flex' : 'none'};
+    //Evaluar si viewoption es true se aplica flex y si es false se aplica none
+    //https://styled-components.com/docs/basics#adapting-based-on-props
+    display: ${(props) => (props.viewOption ? "flex" : "none")};
     flex-direction: column;
-    width: 100%;
     height: 98vh;
-    font-family: 'Josefin Sans', sans-serif;
+    font-family: "Josefin Sans", sans-serif;
     font-size: 2rem;
     font-weight: 300;
-    padding: inherit;
-    margin-left: -115px;
+    margin: 80px;
+    margin-left: -40px;
+    text-align: left;
   }
 `;
 
-export const IconHamburger = styled.figure `
-    display: none;
-  @media screen and (max-width:376px){
+export const IconHamburger = styled.figure`
+  display: none;
+  @media screen and (max-width: 376px) {
     display: block;
     color: white;
     font-size: 1rem;
     position: absolute;
     right: 0;
+  }
+  .hamburguesa {
+    position: relative;
+    left: -20px;
+    top: 2px;
   }
 `;
